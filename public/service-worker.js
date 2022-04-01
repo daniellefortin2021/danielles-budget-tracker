427cd1d15445689acc86ff77f7227d937c5c5e4b
Danielle Fortin <fortind6@gmail.com>
2022-04-01 16:31:26 -0400

    updated service-worker with icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,19 @@ const CACHE_NAME = 'budget-tracker-cache';
 const DATA_CACHE_NAME = 'data-budget-tracker-cache';
 
 const FILES_TO_CACHE = [ 
-    "./index.html",
-    "./css/style.css",
-    "./js/idb.js",
-    "./js/index.js"
+    "/index.html",
+    "/css/style.css",
+    "/js/idb.js",
+    "/js/index.js",
+    "/manifest.json",
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ]
 
 self.addEventListener('install', function(evt) {
@@ -61,7 +70,18 @@ self.addEventListener('fetch', function(evt) {
   
       return;
     }
-    
+    evt.respondWith(
+      fetch(evt.request).catch(function() {
+        return caches.match(evt.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (evt.request.headers.get('accept').includes('text/html')) {
+            // return the cached home page for all requests for html pages
+            return caches.match('/');
+          }
+        });
+      })
+    );
   });
   
   
